865d2be57be8d7de381476df006478d356cf07b1
Jaloliddin <130665265+jaloliddin20007@users.noreply.github.com>
2023-07-15 09:36:00 +0500

    ColorChanger

---
+++ app.js
@@ -0,0 +1,13 @@
+const name = document.querySelector(".name");
+
+function changeColor(color) {
+   name.style.color = color;
+}
+
+function chBackcolor(color) {
+   document.body.style.background = color;
+}
+
+function chBackcolor(color) {
+   document.body.style.background = color;
+}
